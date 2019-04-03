f8c4a7b6dc059cc9991ccd99238059e779edbdcf
Raj Dipakkumar Patel <rajdipakkumar.patel@factset.com>
2019-04-03 10:10:59 +0530

    feat(router): setting up router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import HomePage from './components/HomePage.vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter);
+
+const routes = [
+  {path: '/', component: HomePage}
+]
+
+const router = new VueRouter({
+  routes
+})
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
